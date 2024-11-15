59a30d341476cbbfb8cb088c324c4183973a1943
walied <waliednapil37@gmail.com>
2024-11-15 17:50:01 +0200

    + settings tab

---
+++ lib/ui/home/settings_tab/language_bottom_sheet.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LanguageBottomSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          getSelectedItem('English'),
+          SizedBox(
+            height: 18,
+          ),
+          getUnSelectedItem('Arabic'),
+        ],
+      ),
+    );
+  }
+
+  Widget getSelectedItem(String text) {
+    return Row(
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFB7935F)),
+        ),
+        Spacer(),
+        Icon(
+          (Icons.check),
+          color: Color(0xFFB7935F),
+          size: 28,
+          weight: 28,
+        ),
+      ],
+    );
+  }
+
+  Widget getUnSelectedItem(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFFB7935F)),
+    );
+  }
+}
